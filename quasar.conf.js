ef14db3070cf21e1e59fd721f309ec9ca83c8890
Larissa Moro <54962550+LariMoro20@users.noreply.github.com>
2023-03-17 00:39:47 -0300

    Update quasar.conf.js

--- quasar.conf.js
+++ quasar.conf.js
@@ -140,41 +140,40 @@ module.exports = configure(function (ctx) {
       },
 
       manifest: {
-        name: `Pokedex`,
-        short_name: `Pokedex`,
+        name: `Pokemon App`,
+        short_name: `Pokemon App`,
         description: `A Quasar Framework app`,
-        display: "standalone",
-        orientation: "portrait",
-        background_color: "#ffffff",
-        theme_color: "#027be3",
-        icons: [
-          {
-            src: "icons/icon-128x128.png",
-            sizes: "128x128",
-            type: "image/png",
-          },
-          {
-            src: "icons/icon-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "icons/icon-256x256.png",
-            sizes: "256x256",
-            type: "image/png",
-          },
-          {
-            src: "icons/icon-384x384.png",
-            sizes: "384x384",
-            type: "image/png",
-          },
-          {
-            src: "icons/icon-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
+        display: 'standalone',
+        orientation: 'portrait',
+        background_color: '#C10015',
+        theme_color: '#b71c1c',
+        icons: [{
+          src: 'icons/icon-128x128.png',
+          sizes: '128x128',
+          type: 'image/png'
+        },
+        {
+          src: 'icons/icon-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'icons/icon-256x256.png',
+          sizes: '256x256',
+          type: 'image/png'
+        },
+        {
+          src: 'icons/icon-384x384.png',
+          sizes: '384x384',
+          type: 'image/png'
+        },
+        {
+          src: 'icons/icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+        ]
+      }
     },
 
     // Full list of options: https://quasar.dev/quasar-cli/developing-cordova-apps/configuring-cordova
